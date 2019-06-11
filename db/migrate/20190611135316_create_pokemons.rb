3f364880705313605784d58f38079901182b45c9
Renan Rudney <renanrudney@gmail.com>
2019-06-11 11:37:59 -0300

    Module API/V1

---
+++ db/migrate/20190611135316_create_pokemons.rb
@@ -0,0 +1,12 @@
+class CreatePokemons < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.decimal :height
+      t.decimal :weight
+      t.string :specie
+
+      t.timestamps
+    end
+  end
+end
